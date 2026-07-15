b44e31f684a45d538a5280ed28e7714ec73bb29f
Tyler Durham <TylerDurham@users.noreply.github.com>
2026-07-14 21:01:12 -0500

    feat(home/git.nix): add GPG signing configuration for Git
    * modules/home/git.nix: set user.signingkey for GPG signing
    * modules/home/git.nix: configure gpg.format to "ssh"
    * modules/home/git.nix: specify the GPG program as op-ssh-sign
    * modules/home/git.nix: enable commit.gpgsign

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -9,11 +9,16 @@
     settings = {
       user.name = "Tyler Durham";
       user.email = "TylerDurham@noreply.users.github.com";
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIF0MyXTqMTH4SoUBocralanLGXtymmBxTc5t7cwi9mI";
 
       init.defaultBranch = "master";
       pull.rebase = true;
       push.autoSetupRemote = true;
       core.editor = "nvim";
+
+      gpg.format = "ssh";
+      "gpg \"ssh\"".program = "/run/current-system/sw/bin/op-ssh-sign";
+      commit.gpgsign = true;
     };
 
   };
