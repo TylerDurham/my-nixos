aa079daa5dfeb81a3d649af0511b5c6dea1392fd
Tyler Durham <TylerDurham@users.noreply.github.com>
2026-07-17 11:29:21 -0500

    chore(nix): remove unused grc configuration files (moved to my-shell)
    * modules/home/development/conf.gotest: delete unused go-test colorizer configuration file
    * modules/home/development/go.nix: comment out references to removed conf.gotest and grc.conf files
    * modules/home/development/grc.conf: delete unused main grc rules file

--- modules/home/development/go.nix
+++ modules/home/development/go.nix
@@ -9,7 +9,7 @@
     ];
 
     # grc config: main rules file + the go test colorizer it references
-    home.file.".grc/grc.conf".source = ./grc.conf;
-    home.file.".grc/conf.gotest".source = ./conf.gotest;
+    # home.file.".grc/grc.conf".source = ./grc.conf;
+    # home.file.".grc/conf.gotest".source = ./conf.gotest;
   };
 }
