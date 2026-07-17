ef735dbbf75686aadcfd5be5f5f52eabef46a5f0
Tyler Durham <TylerDurham@users.noreply.github.com>
2026-07-17 11:07:30 -0500

    feat: add go-test colorizer configuration
    * modules/home/development/conf.gotest: add grc colorizer configuration for go tests
    * modules/home/development/go.nix: include grc and the new colorizer config in home setup
    * modules/home/development/grc.conf: add main grc rules file referencing the go test colorizer

--- modules/home/development/go.nix
+++ modules/home/development/go.nix
@@ -5,6 +5,11 @@
       pkgs.go
       pkgs.gopls
       pkgs.gotools
+      pkgs.grc
     ];
+
+    # grc config: main rules file + the go test colorizer it references
+    home.file.".grc/grc.conf".source = ./grc.conf;
+    home.file.".grc/conf.gotest".source = ./conf.gotest;
   };
 }
