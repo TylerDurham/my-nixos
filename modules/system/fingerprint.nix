6b2116a9359b0623e99fddf9421de6eb0c4e3f1d
Tyler Durham <TylerDurham@users.noreply.github.com>
2026-07-18 12:03:19 -0500

    fix(system/fingerprint): disable USB autosuspend for Goodix reader
    * modules/system/fingerprint.nix: add udev rule to keep the Goodix fingerprint reader powered on

--- modules/system/fingerprint.nix
+++ modules/system/fingerprint.nix
@@ -15,5 +15,12 @@ in
 
     security.pam.services.hyprlock.fprintAuth = true;
     security.pam.services."1password".fprintAuth = true;
+
+    # The Goodix reader (27c6:609c) autosuspends after 2s idle and often fails
+    # to reinitialize on wake, so fingerprint auth silently stops working after
+    # the machine has been idle for a while. Disable USB autosuspend for it.
+    services.udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="27c6", ATTR{idProduct}=="609c", TEST=="power/control", ATTR{power/control}="on"
+    '';
   };
 }
